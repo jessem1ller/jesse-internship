439b447d579bbf61b9ff6abd19875dd549727f7c
Jesse Miller <silverhonda84@gmail.com>
2025-05-24 12:16:43 -0700

    Update the app.jsx page to accept dynamic route and made author and author items pages dynamic

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import ItemsGridSkeletonLoader from '../reusable/ItemsGridSkeletonLoader';
 
-const AuthorItems = () => {
+const AuthorItems = ({ items = [], authorCardImage, loading }) => {
+  if (loading) {
+    return (
+      <div className="de_tab_content">
+        <div className="tab-1">
+          <div className="row">
+            <ItemsGridSkeletonLoader
+              count={8}
+              itemClassName="p-2 col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              skeletonHeight="350px"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (!items || items.length === 0) {
+    return <div className="text-center mt-5">No items found for this author.</div>;
+  }
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {items.map((item, index) => (
+            <div
+              key={item.nftId || index}
+              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
               <div className="nft__item">
                 <div className="author_list_pp">
-                  <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                  <Link
+                    to={`/author/${item.authorId}`}
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title={`Creator: ${item.authorId}`}
+                  >
+                    <img className="lazy" src={authorCardImage} alt="Author" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -23,34 +51,30 @@ const AuthorItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="#" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="#" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="#">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <img src={item.nftImage} className="lazy nft__item_preview" alt={item.title} />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
@@ -63,3 +87,76 @@ const AuthorItems = () => {
 };
 
 export default AuthorItems;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const AuthorItems = ({ items = [], authorCardImage }) => {
+//   if (!items || items.length === 0) {
+//     return <div className="text-center mt-5">No items found for this author.</div>;
+//   }
+
+//   return (
+//     <div className="de_tab_content">
+//       <div className="tab-1">
+//         <div className="row">
+//           {items.map((item, index) => (
+//             <div
+//               key={item.nftId || index}
+//               className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+//               style={{ display: "block", backgroundSize: "cover" }}
+//             >
+//               <div className="nft__item">
+//                 <div className="author_list_pp">
+//                   <Link
+//                     to={`/author/${item.authorId}`}
+//                     data-bs-toggle="tooltip"
+//                     data-bs-placement="top"
+//                     title={`Creator: ${item.authorId}`}
+//                   >
+//                     <img className="lazy" src={authorCardImage} alt="Author" />
+//                     <i className="fa fa-check"></i>
+//                   </Link>
+//                 </div>
+//                 <div className="nft__item_wrap">
+//                   <div className="nft__item_extra">
+//                     <div className="nft__item_buttons">
+//                       <button>Buy Now</button>
+//                       <div className="nft__item_share">
+//                         <h4>Share</h4>
+//                         <a href="#" target="_blank" rel="noreferrer">
+//                           <i className="fa fa-facebook fa-lg"></i>
+//                         </a>
+//                         <a href="#" target="_blank" rel="noreferrer">
+//                           <i className="fa fa-twitter fa-lg"></i>
+//                         </a>
+//                         <a href="#">
+//                           <i className="fa fa-envelope fa-lg"></i>
+//                         </a>
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <Link to={`/item-details/${item.nftId}`}>
+//                     <img src={item.nftImage} className="lazy nft__item_preview" alt={item.title} />
+//                   </Link>
+//                 </div>
+//                 <div className="nft__item_info">
+//                   <Link to={`/item-details/${item.nftId}`}>
+//                     <h4>{item.title}</h4>
+//                   </Link>
+//                   <div className="nft__item_price">{item.price} ETH</div>
+//                   <div className="nft__item_like">
+//                     <i className="fa fa-heart"></i>
+//                     <span>{item.likes}</span>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AuthorItems;
